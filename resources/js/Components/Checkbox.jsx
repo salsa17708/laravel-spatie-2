11986c765f759de94279e9243a9d861311f57102
salsa17708 <salsabila17708@gmail.com>
2025-01-16 20:34:15 +0700

    Component Checkbox

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -10,3 +10,19 @@ export default function Checkbox({ className = '', ...props }) {
         />
     );
 }
+export default function Checkbox({ label, ...props }) {
+    return (
+        <div>
+            <div className="flex flex-row items-center gap-2">
+                <input
+                    {...props}
+                    type="checkbox"
+                    className={
+                        "rounded-md bg-white border-gray-200 checked:bg-teal-500"
+                    }
+                />
+                <label className="text-sm text-gray-700">{label}</label>
+            </div>
+        </div>
+    );
+}
